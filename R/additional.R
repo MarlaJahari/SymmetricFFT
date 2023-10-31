55ff9e3d0abe02ab895ab93dfe78e4016cbef439
MarlaJahari <sciencefan19@gmail.com>
2023-11-01 05:17:41 +0530

    tinier operational functions building up to main functions

--- R/additional.R
+++ R/additional.R
@@ -1,24 +1,24 @@
 #find the indices of partitions in the Pd array corresponding to the partitions in the PDA array
 #searching for each partition in PDA within the Pd array
 
-dia1 <- function(PDA, Pd, lb) {
-  PDA_L <- length(PDA)
-  DIA <- numeric(PDA_L)
-  c <- 1
-  p <- lb
-  while (TRUE) {
-    if (identical(Pd[[p]], PDA[[c]])) {
-      DIA[c] <- p
-      c <- c + 1
-      if (c > PDA_L) {
+dia1<- function(PDA, Pd, lb) {
+  PDA_L<- length(PDA)
+  DIA<- numeric(PDA_L)
+  c<- 1
+  p<- lb
+  while(TRUE) {
+    if(identical(Pd[[p]], PDA[[c]])) {
+      DIA[c]<- p
+      c<- c+1
+      if(c>PDA_L){
         return(DIA)
       }
     }
-    p <- p+1
+    p<- p+1
   }
 }
 
-dia2<- function(PDA, Pd, lb1, lb2) {
+dia2<-function(PDA, Pd, lb1, lb2) {
   PDA_L<-length(PDA)
   DIA<-integer(PDA_L)
 
@@ -35,20 +35,20 @@ dia2<- function(PDA, Pd, lb1, lb2) {
   while(TRUE) {
     if (identical(Pd[[p]], PDA[[c]])) {
       DIA[c]<-p
-      c<- c+1
-      if (c>PDA_L) {
+      c<-c+1
+      if(c>PDA_L) {
         return(DIA)
       }
     }
-    p<- p+1
+    p<-p+1
   }
 }
 
 #generating a partition decomposition array PDA from a given partition P
 #detects where the partition P splits into smaller partitions based on the values in the partition
 pda<- function(P) {
-  P_L<- length(P)
-  num<- 1
+  P_L<-length(P)
+  num<-1
   for(i in 1:(P_L - 1)) {
     if(P[i] > P[i + 1]) {
       num<- num + 1
@@ -57,14 +57,14 @@ pda<- function(P) {
   PDA<- vector("list", num)
   c<- 1
   for(i in 1:(P_L - 1)) {
-    if(P[i] > P[i + 1]) {
-      D <- as.integer(P)
-      D[i] <- D[i] - 1
-      PDA[[c]] <- D
-      c <- c + 1
+    if(P[i]>P[i + 1]) {
+      D<-as.integer(P)
+      D[i]<- D[i]-1
+      PDA[[c]]<-D
+      c <- c+1
     }
   }
-  if(P[P_L] == 1) {
+  if(P[P_L]==1) {
     D<- as.integer(P[1:(P_L - 1)])
     PDA[[c]] <- D
   } else{
@@ -123,26 +123,26 @@ partitions<-function(N) {
   WI<-matrix(0, nrow=N,ncol=N)
 
   for(n in 1:N) {
-    WI[n,1]<- 1
+    WI[n,1]<-1
   }
 
   for(w in 2:N) {
-    WI[w,w]<- 1
+    WI[w,w]<-1
 
-    for(n in (w + 1):N) {
+    for(n in (w+1):N) {
       num<-0
 
       for(i in 1:min(n-w, w)) {
         num<- num+WI[n-w,i]
       }
 
-      WI[n,w]<- num
+      WI[n,w]<-num
     }
   }
 
   for(n in 2:N) {
     for(w in 2:n) {
-      WI[n, w]<- WI[n, w]+WI[n, w - 1]
+      WI[n, w]<-WI[n, w]+WI[n, w - 1]
     }
   }
 
@@ -156,8 +156,8 @@ partitions<-function(N) {
 
     for (w in 2:(n - 1)) {
       for (spi in 1:WI[n - w, min(n - w, w)]) {
-        Pn[[i]] <- c(w, P[[n - w]][[spi]])
-        i <- i + 1
+        Pn[[i]]<- c(w, P[[n - w]][[spi]])
+        i<- i+1
       }
     }
 
